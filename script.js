948d7ed08d0d8ea590b24bfc53eff480bc74771b
zlatanatan <natalyzoloto@gmail.com>
2016-11-18 00:02:58 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -51,7 +51,7 @@ WARNING!!! WARNING!!!
         // string object's 'toLowerCase' method on the result so we can compare
         // to lower case character 'j' afterwards.
         // Look up these methods on Mozilla Developer Network web site if needed.
-        var firstLetter = names[i].charAt(1).toLowerCase;
+        var firstLetter = names[i].charAt(1).toLowerCase();
 
         // STEP 12:
         // Compare the 'firstLetter' retrieved in STEP 11 to lower case
